b74a08ac57eae2d3c91b276ee1425997e4ce910c
Viholialves <162927447+Viholialves@users.noreply.github.com>
2024-09-23 18:31:42 -0300

    first commit

---
+++ java/areausu.js
@@ -0,0 +1,18 @@
+// Função para navegar para as páginas das opções
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+// Função para encerrar a sessão do usuário
+function logout() {
+    const confirmation = confirm('Tem certeza de que deseja encerrar a sessão?');
+    if (confirmation) {
+        // Simula o encerramento da sessão (adicione a lógica real de logout aqui)
+        alert('Sessão encerrada com sucesso.');
+        window.location.href = '../index.html'; // Redireciona para a página de login ou inicial
+    }
+}
+// Função para voltar para a página anterior
+function goBack() {
+    window.history.back();
+}
